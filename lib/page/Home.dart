cc91a71678ec922bc444b7da891ed96b013e0a1f
suchanun6104101393 <zadkee27@gmail.com>
2021-10-08 14:31:21 +0700

    apioutlaw

--- lib/page/Home.dart
+++ lib/page/Home.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
 
   var _dataFromAPI = [];
 
-  void _getnews() async {
+  Future<String> _getnews() async {
     //var url = Uri.parse("https://wwwdev.csmju.com/api/news");
     try {
       final response = await http.get(url);
@@ -36,7 +36,7 @@ class _HomePageState extends State<HomePage> {
     //_dataFromAPI = apinewFromJson(news.body) as Apinew;
     //print(jsonData);
 
-    //return "ok";
+    return "ok";
   }
 
   @override
@@ -68,13 +68,44 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         body: Center(
-            child: ListView.builder(
-                itemCount: _dataFromAPI.length,
-                itemBuilder: (context, i) {
-                  final post = _dataFromAPI[i];
-                  return Text(
-                      "newsId:${post["newsId"]}\n News_Detail : ${post["News_Detail"]}");
-                })));
+          child: FutureBuilder(
+            future: _getnews(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.error != null) {
+                return Text("error");
+                //child: Text("newsId:${post["newsId"]} \n News_Detail : ${post["News_Detail"]}"),
+              }
+              if (snapshot.connectionState == ConnectionState.done) {
+                return ListView.builder(
+                    itemCount: _dataFromAPI.length,
+                    itemBuilder: (context, i) {
+                      final post = _dataFromAPI[i];
+                      return SizedBox(
+                 
+
+                        width: 206,
+                        height: 222,
+                        child: Material(
+                          child: Text(":${post["newsId"]} \n  : ${post["News_Detail"]}"),
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              width: 2,
+                              color: Color(0xffd1cccc),
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      );
+                    });
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+        ));
 
     //child: Text(_dataFromAPI?.newsDetail ?? "loadind..."),
   }
